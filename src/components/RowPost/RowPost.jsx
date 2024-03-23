4bb46e4acfb4d832b2bb69ed635b88db06bdf222
Allen John <allenjohnmonapallil@gmail.com>
2024-03-23 11:17:43 +0530

    add: yt final

--- src/components/RowPost/RowPost.jsx
+++ src/components/RowPost/RowPost.jsx
@@ -22,6 +22,8 @@ function RowPost(props) {
   }, [])
 
   const opts = {
+    height: '390',
+      width: '100%',
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 0,
